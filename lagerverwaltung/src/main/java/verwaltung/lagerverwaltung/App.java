cc2770f20079ee3d05319831fdd3007dd49ad861
Grasi310 <m.grassmayr@gmx.net>
2018-10-11 14:45:38 +0200

    First Commit

---
+++ lagerverwaltung/src/main/java/verwaltung/lagerverwaltung/App.java
@@ -0,0 +1,13 @@
+package verwaltung.lagerverwaltung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
